0f9016c05db9cf4298de8b44ec1d05493105978d
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-05-17 10:33:15 -0500

    componente - comun

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -3,15 +3,24 @@ import { useProduct } from "../hooks/useProduct";
 import styles from "../styles/styles.module.css";
 import noImage from "../assets/no-image.jpg";
 
-export const ProductCard = () => {
+interface MyProps {
+    product: Product;
+}
+export interface Product {
+    id: string;
+    title: string;
+    img?: string;
+}
+
+export const ProductCard = ({ product }: MyProps) => {
     // console.log(styles);
     const { counter, increaseBy } = useProduct();
 
     return (
         <div className={styles.productCard}>
             {/* <img className={styles.productImg} src={"./coffee-mug.png"} alt="Coffe Mug" /> */}
-            <img className={styles.productImg} src={noImage} alt="Coffe Mug" />
-            <span className={styles.productDescription}>Coffee Mug</span>
+            <img className={styles.productImg} src={product.img ?? noImage} alt="Coffe Mug" />
+            <span className={styles.productDescription}>{product.title}</span>
             <div className={styles.buttonsContainer}>
                 <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}
                 > - </button>
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,11 +1,26 @@
-import { ProductCard } from "../components/ProductCard"
+import { Product, ProductCard } from "../components/ProductCard"
+
+const product: Product = {
+    id: "1",
+    title: "Coffee Mug",
+    img: "./coffee-mug.png"
+}
 
 export const ShoppingPage = () => {
     return (
         <div>
             <h1>ShoppingPage</h1>
             <hr />
-            <ProductCard />
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                }}
+            >
+                <ProductCard product={product} />
+
+            </div>
         </div>
     )
 }
