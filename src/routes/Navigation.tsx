cc5b74b477f7a4f8d134a74a31164236a8c3294a
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-05-17 10:17:47 -0500

    add: custom hook useProduct

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
 
 import logo from '../logo.svg'
 
@@ -8,27 +9,27 @@ export const Navigation = () => {
         <BrowserRouter>
             <div className="main-layout">
                 <nav>
-                    <img src={ logo } alt="React Logo" />
+                    <img src={logo} alt="React Logo" />
                     <ul>
                         <li>
-                            <NavLink to="/home" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Home</NavLink>
+                            <NavLink to="/shoppingPage" className={({ isActive }) => isActive ? 'nav-active' : ''}>ShoppingPage</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/about" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
+                            <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/users" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Users</NavLink>
+                            <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>
                         </li>
                     </ul>
                 </nav>
 
 
                 <Routes>
-                    <Route path="about" element={ <h1>About Page</h1> } />
-                    <Route path="users" element={ <h1>Users Page</h1> } />
-                    <Route path="home" element={ <h1>Home Page</h1> } />
-                    
-                    <Route path="/*" element={ <Navigate to="/home" replace /> } />
+                    <Route path="about" element={<h1>About Page</h1>} />
+                    <Route path="users" element={<h1>Users Page</h1>} />
+                    <Route path="shoppingPage" element={<ShoppingPage />} />
+
+                    <Route path="/*" element={<Navigate to="/shoppingPage" replace />} />
                 </Routes>
 
             </div>
