7c5c4f7662c15b70c8c13a8a96dbef1f7a48d33d
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-05-17 10:17:52 -0500

    add: custom hook useProduct

---
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,26 @@
+import { useProduct } from "../hooks/useProduct";
+
+import styles from "../styles/styles.module.css";
+import noImage from "../assets/no-image.jpg";
+
+export const ProductCard = () => {
+    // console.log(styles);
+    const { counter, increaseBy } = useProduct();
+
+    return (
+        <div className={styles.productCard}>
+            {/* <img className={styles.productImg} src={"./coffee-mug.png"} alt="Coffe Mug" /> */}
+            <img className={styles.productImg} src={noImage} alt="Coffe Mug" />
+            <span className={styles.productDescription}>Coffee Mug</span>
+            <div className={styles.buttonsContainer}>
+                <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}
+                > - </button>
+
+                <div className={styles.countLabel}>{counter}</div>
+                <button className={styles.buttonAdd}
+                    onClick={() => increaseBy(+1)}
+                > + </button>
+            </div>
+        </div>
+    )
+}
---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,11 @@
+import { ProductCard } from "../components/ProductCard"
+
+export const ShoppingPage = () => {
+    return (
+        <div>
+            <h1>ShoppingPage</h1>
+            <hr />
+            <ProductCard />
+        </div>
+    )
+}
